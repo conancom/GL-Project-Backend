f806bace0c9f32c391429cab6ebb7fb2db6e2571
conancom <cnoanplays@gmail.com>
2022-09-08 19:05:32 +0700

    Relationship fix

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # GL-Project-Backend
+
+### Set Up
+
+#### Initialize Database
+1) Have Postgres installed. (or Use Docker's Postgres Image)
+2) Create a new Database called ```ProjectGL```
+3) Execute the DDL query in ```./DB/db_dump.sql```
--- src/main/java/com/projectgl/backend/PersonalGameInformation/PersonalGameInformation.java
+++ src/main/java/com/projectgl/backend/PersonalGameInformation/PersonalGameInformation.java
@@ -1,4 +1,13 @@
 package com.projectgl.backend.PersonalGameInformation;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "personalgameinformation")
 public class PersonalGameInformation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
