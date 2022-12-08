862e8f9cc38c36c24b669da07e9560e27ef116c0
conancom <cnoanplays@gmail.com>
2022-12-08 23:08:20 +0700

    more on sec

--- src/main/java/com/projectgl/backend/Security/SecurityConfig.java
+++ src/main/java/com/projectgl/backend/Security/SecurityConfig.java
@@ -1,19 +1,21 @@
 package com.projectgl.backend.Security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+@Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().sessionManagement().sessionFixation().none().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .sessionManagement().sessionFixation().none()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
 
