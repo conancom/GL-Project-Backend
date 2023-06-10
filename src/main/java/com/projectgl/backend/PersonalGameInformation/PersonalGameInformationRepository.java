371973e8696729131c70931dcf4f6567402c18a7
conancom <cnoanplays@gmail.com>
2023-06-10 22:17:22 +0700

    add transactional to catcht lob problem

--- src/main/java/com/projectgl/backend/PersonalGameInformation/PersonalGameInformationRepository.java
+++ src/main/java/com/projectgl/backend/PersonalGameInformation/PersonalGameInformationRepository.java
@@ -4,9 +4,11 @@ import com.projectgl.backend.RegisteredLibraryAccount.RegisteredLibraryAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 
 @Repository
 public interface PersonalGameInformationRepository extends JpaRepository<PersonalGameInformation, Long> {
+    @Transactional
     ArrayList<PersonalGameInformation> getPersonalGameInformationByRegisteredLibraryAccount(RegisteredLibraryAccount registeredLibraryAccount);
 }
