21e123db4a6456a36804ef381e5464fb78fdd198
conancom <cnoanplays@gmail.com>
2023-06-10 22:55:03 +0700

    add cors config for front end

---
+++ src/main/java/com/projectgl/backend/Security/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.projectgl.backend.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://gl-project.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
